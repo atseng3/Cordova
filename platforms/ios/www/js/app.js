1299d72e729c9e9e1e44b6e105919e5ca796c1d4
Albert Tseng <atseng3@gmail.com>
2014-02-01 17:03:58 -0800

    Part 6: Avoid the 300ms Click Delay

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -14,18 +14,19 @@
         alert("Some help here...")
     });
 		
-    // document.addEventListener('deviceready', function () {
-    //     if (navigator.notification) { // Override default HTML alert with native dialog
-    //         window.alert = function (message) {
-    //             navigator.notification.alert(
-    //                 message,    // message
-    //                 null,       // callback
-    //                 "Workshop", // title
-    //                 'OK'        // buttonName
-    //             );
-    //         };
-    //     }
-    // }, false);
+    document.addEventListener('deviceready', function () {
+			FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
