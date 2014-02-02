e465c0e814083e77aeb192d97c06c31181212f66
Albert Tseng <atseng3@gmail.com>
2014-02-02 15:19:50 -0800

    Ep12 Using the Location API

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -27,6 +27,34 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -34,7 +62,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.device": "0.2.7",
     "org.apache.cordova.console": "0.2.6",
-    "org.apache.cordova.dialogs": "0.2.5"
+    "org.apache.cordova.dialogs": "0.2.5",
+    "org.apache.cordova.geolocation": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
